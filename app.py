422a30ef216da922fd39e7777938d165b81ef875
Therushmorare <tshego.tt.morare@gmail.com>
2023-08-19 02:04:08 +0200

    app

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request, url_for, redirect,send_from_directory, jsonify
 
 app = Flask(__name__)
 
@@ -6,6 +6,12 @@ app = Flask(__name__)
 def index():
     return render_template('app.html')
 
+@app.route('/submitted_field',methods=["GET","POST"])
+def submitted_field():
+    if request.method == "POST":
+        file_name = request.form.get('file_name')
+        if file_name is None:
+            return render_template('app.html', err='no file')
+    return render_template('app.html',err=file_name)
 if __name__ == '__main__':
     app.run(debug=True)
-
